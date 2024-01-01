d18d03e912c40cce6d5f69f271fd7f184c88aae4
L0Z1K <qortmddbs731@korea.ac.kr>
2024-01-01 15:27:58 +0900

    Finish HW7

--- hw7/main.py
+++ hw7/main.py
@@ -1,5 +1,14 @@
+from collections import namedtuple
 import itertools
 import random
+from itertools import count
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Categorical
 
 
 class BlackJack:
@@ -121,47 +130,158 @@ class MonteCarlo:
         return actions[Qs.index(max(Qs))]  # greedy
 
 
-if __name__ == "__main__":
-    game = BlackJack()
+class Policy(nn.Module):
+    def __init__(self):
+        super(Policy, self).__init__()
+        self.affine1 = nn.Linear(3, 128)
 
-    agent = MonteCarlo()
+        # actor's layer
+        self.action_head = nn.Linear(128, 2)
 
-    for epoch in range(1000):
+        # critic's layer
+        self.value_head = nn.Linear(128, 1)
+
+        # action & reward buffer
+        self.saved_actions = []
+        self.rewards = []
+
+    def forward(self, x):
+        """
+        forward of both actor and critic
+        """
+        x = F.relu(self.affine1(x))
+
+        action_prob = F.softmax(self.action_head(x), dim=-1)
+
+        state_values = self.value_head(x)
+
+        return action_prob, state_values
+
+
+model = Policy()
+optimizer = optim.Adam(model.parameters(), lr=3e-3)
+eps = np.finfo(np.float32).eps.item()
+SavedAction = namedtuple("SavedAction", ["log_prob", "value"])
+
+
+def select_action(state):
+    state = torch.from_numpy(state).float()
+    probs, state_value = model(state)
+
+    # create a categorical distribution over the list of probabilities of actions
+    m = Categorical(probs)
+
+    # and sample an action using the distribution
+    action = m.sample()
+
+    # save to action buffer
+    model.saved_actions.append(SavedAction(m.log_prob(action), state_value))
+
+    # the action to take (left or right)
+    return action.item()
+
+
+def finish_episode():
+    """
+    Training code. Calculates actor and critic loss and performs backprop.
+    """
+    R = 0
+    saved_actions = model.saved_actions
+    policy_losses = []  # list to save actor (policy) loss
+    value_losses = []  # list to save critic (value) loss
+    returns = []  # list to save the true values
+
+    # calculate the true value using rewards returned from the environment
+    for r in model.rewards[::-1]:
+        # calculate the discounted value
+        R = r + 0.99 * R
+        returns.insert(0, R)
+
+    returns = torch.tensor(returns)
+    returns = (returns - returns.mean()) / (returns.std() + eps)
+
+    for (log_prob, value), R in zip(saved_actions, returns):
+        advantage = R - value.item()
+
+        # calculate actor (policy) loss
+        policy_losses.append(-log_prob * advantage)
+
+        # calculate critic (value) loss using L1 smooth loss
+        value_losses.append(F.smooth_l1_loss(value, torch.tensor([R])))
+
+    # reset gradients
+    optimizer.zero_grad()
+
+    # sum up all the values of policy_losses and value_losses
+    loss = torch.stack(policy_losses).sum() + torch.stack(value_losses).sum()
+
+    # perform backprop
+    loss.backward()
+    optimizer.step()
+
+    # reset rewards and action buffer
+    del model.rewards[:]
+    del model.saved_actions[:]
+
+
+def main():
+    srs = []
+    for i_episode in count(1):
+        game = BlackJack()
         success_rate = 0
-        for _ in range(10000):
+        for _ in range(300):
             dealer, player = game.draw_cards()
             dealer_first_card = dealer[0]
 
-            histories = []
+            cnt = 0
             while True:
                 count_aces = sum([1 if card[1] == "A" else 0 for card in player])
-                state = (
-                    count_aces,
-                    game.calculate_card_value(player),
-                    game.calculate_card_value([dealer_first_card]),
+                state = np.array(
+                    [
+                        count_aces,
+                        game.calculate_card_value(player) / 21,
+                        game.calculate_card_value([dealer_first_card]) / 21,
+                    ]
                 )
-                action = agent.get_action(
-                    state,
-                    ["hit", "stand"],
-                )
-                histories.append((state, action))
-                if action == "stand":
+                action = select_action(state)
+                cnt += 1
+                if action == 0:
                     break
-                else:
+                elif action == 1:
                     player.append(game.cards.pop())
                     if game.calculate_card_value(player) > 21:
                         break
+                else:
+                    raise ValueError("Invalid action!", action)
 
             if game.check_winner(player, dealer):
                 reward = 1
             else:
                 reward = -1
-
-            for state, action in histories:
-                agent.update_Q(state, action, reward)
-
+            model.rewards += [reward] * cnt
             success_rate += reward
+        finish_episode()
+        success_rate = (success_rate + 300) / 600
+        srs.append(success_rate)
+        print(f"Episode: {i_episode}, Success rate: {success_rate:.2%}")
+        if i_episode == 200:
+            # save the model
+            torch.save(model.state_dict(), "./model.pt")
+            break
 
-        success_rate = (success_rate + 10000) / 20000
-        print(f"Epoch: {epoch+1}, Success rate: {success_rate:.2%}")
-        agent.store_Q("Q.txt")
+    # plot the success rate and cumulative success rate
+    import matplotlib.pyplot as plt
+
+    plt.plot(srs)
+    plt.xlabel("Episode")
+    plt.ylabel("Success rate")
+    plt.savefig("./success_rate.png")
+    plt.clf()
+    plt.plot(np.cumsum(srs) / np.arange(1, len(srs) + 1))
+    plt.xlabel("Episode")
+    plt.ylabel("Cumulative success rate")
+    plt.savefig("./cumulative_success_rate.png")
+
+
+if __name__ == "__main__":
+    main()
